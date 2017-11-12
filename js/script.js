c2b195a32113442623c19cee0ce9cebeb1d541d2
Minoru <miyarappo@gmail.com>
2017-11-12 19:54:28 +0900

    Homeページのパララックスの実装 スクロールに応じたコンテキストメニューの色変更処理

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 window.addEventListener('DOMContentLoaded', function() {
 
+  //ハンバーガーメニューの閉会
   document.querySelector('#toggle').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('#overlay').classList.toggle('open');
+    document.querySelectorAll('#toggle span').forEach(function(value){
+      value.style.backgroundColor = '#ffffff';
+    });
+    changeHambergerColor();
   }, false);
-  
+
+  // スクロール量に応じてハンバーガーメニューの色を変更
+  window.addEventListener('scroll', function() {
+
+    changeHambergerColor();
+
+  }, false);
+
+  function changeHambergerColor() {
+
+    var scrollHeight = window.pageYOffset;
+    var targetElem = document.querySelector('.project-details').offsetTop;
+
+    if ((scrollHeight + 48) > targetElem) {
+      document.querySelectorAll('#toggle span').forEach(function(value){
+        value.style.backgroundColor = '#444444';
+      });
+    } else {
+      document.querySelectorAll('#toggle span').forEach(function(value){
+        value.style.backgroundColor = '#ffffff';
+      });
+    }
+  }
+
 }, false);
