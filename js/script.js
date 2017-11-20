3f566a129a2eb8b726749905d9d1d5ead40012a9
Minoru <miyarappo@gmail.com>
2017-11-20 19:48:42 +0900

    コンテキストメニューの色が変わってしまうバグを修正

--- js/script.js
+++ js/script.js
@@ -1,25 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-  // $('body').removeClass('fade-wrapper');
+  // 画面遷移アニメーション
   document.querySelector('body').classList.remove('fade-wrapper');
-
-  //ハンバーガーメニューの閉会
-  document.querySelector('#toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('#overlay').classList.toggle('open');
-    document.querySelectorAll('#toggle span').forEach(function(value) {
-      value.style.backgroundColor = '#ffffff';
-    });
-    changeHambergerColor();
-  }, false);
-
-  // スクロール量に応じてハンバーガーメニューの色を変更
-  window.addEventListener('scroll', function() {
-
-    changeHambergerColor();
-
-  }, false);
-
   var anchors = document.querySelectorAll('a:not([href^="#"]):not([target])');
   for (var i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener('click', function(e) {
@@ -34,20 +16,38 @@ window.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  function changeHambergerColor() {
-
-    var scrollHeight = window.pageYOffset;
-    var targetElem = document.querySelector('.change-contextmenu-point').offsetTop;
-
-    if ((scrollHeight + 48) > targetElem) {
-      document.querySelectorAll('#toggle span').forEach(function(value) {
-        value.style.backgroundColor = '#444444';
-      });
-    } else {
-      document.querySelectorAll('#toggle span').forEach(function(value) {
-        value.style.backgroundColor = '#ffffff';
-      });
-    }
-  }
+  //ハンバーガーメニューの閉会
+  document.querySelector('#toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('#overlay').classList.toggle('open');
+    document.querySelectorAll('#toggle span').forEach(function(value) {
+      value.style.backgroundColor = '#ffffff';
+    });
+    changeHambergerColor();
+  }, false);
 
 }, false);
+
+window.addEventListener('scroll', function() {
+  changeHambergerColor();
+}, false);
+
+function changeHambergerColor() {
+
+  if(document.querySelector('#overlay').classList.contains('open')) {
+    return;
+  }
+
+  var scrollHeight = window.pageYOffset;
+  var targetElem = document.querySelector('.change-contextmenu-point').offsetTop;
+
+  if ((scrollHeight + 48) > targetElem) {
+    document.querySelectorAll('#toggle span').forEach(function(value) {
+      value.style.backgroundColor = '#444444';
+    });
+  } else {
+    document.querySelectorAll('#toggle span').forEach(function(value) {
+      value.style.backgroundColor = '#ffffff';
+    });
+  }
+}
