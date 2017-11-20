09571667a260079274272d08692d2c13529bcfdb
Minoru <miyarappo@gmail.com>
2017-11-20 22:08:52 +0900

    gulpタスクにミニファイを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var cleancss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+
 
 gulp.task("sass", function() {
   gulp.src("sass/style.scss")
@@ -8,6 +11,19 @@ gulp.task("sass", function() {
     .pipe(sass())
     .pipe(gulp.dest("css/"));
 });
+
+gulp.task('minify-js', function() {
+  gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'));
+});
+
+gulp.task('minify-css', function() {
+  gulp.src('css/*.css')
+    .pipe(cleancss())
+    .pipe(gulp.dest('css/'));
+});
+
 
 gulp.task("default", function() {
   gulp.watch("sass/*.scss", ["sass"]);
