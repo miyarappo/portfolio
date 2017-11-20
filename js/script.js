e24f72c9b340f31789ffae16fd2f7f416c0e8243
Minoru <miyarappo@gmail.com>
2017-11-20 19:10:22 +0900

    画面遷移時のアニメーション実装

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 window.addEventListener('DOMContentLoaded', function() {
 
+  // $('body').removeClass('fade-wrapper');
+  document.querySelector('body').classList.remove('fade-wrapper');
+
   //ハンバーガーメニューの閉会
   document.querySelector('#toggle').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('#overlay').classList.toggle('open');
-    document.querySelectorAll('#toggle span').forEach(function(value){
+    document.querySelectorAll('#toggle span').forEach(function(value) {
       value.style.backgroundColor = '#ffffff';
     });
     changeHambergerColor();
@@ -17,17 +20,31 @@ window.addEventListener('DOMContentLoaded', function() {
 
   }, false);
 
+  var anchors = document.querySelectorAll('a:not([href^="#"]):not([target])');
+  for (var i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener('click', function(e) {
+      e.preventDefault();
+      var url = this.getAttribute('href');
+      if (url !== '') {
+        document.querySelector('body').classList.add('fade-wrapper');
+        setTimeout(function() {
+          window.location = url;
+        }, 800);
+      }
+    });
+  }
+
   function changeHambergerColor() {
 
     var scrollHeight = window.pageYOffset;
-    var targetElem = document.querySelector('.project-details').offsetTop;
+    var targetElem = document.querySelector('.change-contextmenu-point').offsetTop;
 
     if ((scrollHeight + 48) > targetElem) {
-      document.querySelectorAll('#toggle span').forEach(function(value){
+      document.querySelectorAll('#toggle span').forEach(function(value) {
         value.style.backgroundColor = '#444444';
       });
     } else {
-      document.querySelectorAll('#toggle span').forEach(function(value){
+      document.querySelectorAll('#toggle span').forEach(function(value) {
         value.style.backgroundColor = '#ffffff';
       });
     }
